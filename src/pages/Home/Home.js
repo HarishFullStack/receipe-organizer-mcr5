25da57bc02f49cfc9dfb4028ccfd7e9215c4cdf1
HarishFullStack <harishgupta.dev@gmail.com>
2023-07-01 22:38:34 +0530

    added individual Recipe component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -26,13 +26,16 @@ export function Home(){
             return {...state, instructions: action.value}
 
         case "add":
-            console.log(state);
             setRecipes([...recipes, {id: uuid(), recipeName: state.recipeName, cuisineType: state.cuisineType, ingredients: state.ingredients, instructions: state.instructions}])
-            return state;
-
+            console.log(recipes)
+            break;
+            
         case "clear":
             return {...state, recipeName: "", cuisineType: "", ingredients: "", instructions: ""}
 
+        case "search":
+            return {...state, recipeName: "", cuisineType: "", ingredients: "", instructions: ""}
+    
             default:
             return state;
         }
@@ -47,7 +50,6 @@ export function Home(){
         cuisineType: "",
         ingredients: "",
         instructions: "",
-        recipes: []
     })
 
     const handleDisabled = () => {
@@ -55,69 +57,81 @@ export function Home(){
     }
 
     return (
-        <div className="row">
-        {
-            recipes.map((recipe) => {
-            return(
-                <div key={recipe.id} className="card" style={{width: "18rem"}}>
-                    {/* <img src="..." className="card-img-top" alt="..."></img> */}
-                    <div className="card-body">
-                        <h5 className="card-title">{recipe.recipeName}</h5>
-                        <div>
-                        <p className="card-text"><b>Cuisine Type: </b><span>{recipe.cuisineType}</span></p>
-                        <p className="card-text"><b>Ingredients: </b><span className='cursor-pointer align-self-end' onClick={() => navigate(`/recipe/${recipe.id}`)}>See Recipe <i className="fa fa-chevron-right" aria-hidden="true"></i></span></p>
-                        <p className="card-text"><b>Instructions: </b><span className='cursor-pointer align-self-end'>See Recipe <i className="fa fa-chevron-right" aria-hidden="true"></i></span></p>
-                        </div>
-                    </div>
+        <div>
+            <div className="row" style={{margin: "0.5rem"}}>
+                <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Search the item you want" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+                    <span className="input-group-text" id="basic-addon2">
+                        Filters: <input type="radio" name="filter" onClick={() => dispatch({type: "search", value: "name"})}/> Name
+                        <input type="radio" name="filter" onClick={() => dispatch({type: "search", value: "ingredients"})}/> Ingredients
+                        <input type="radio" name="filter" onClick={() => dispatch({type: "search", value: "cuisine"})}/> Cuisine</span>
                 </div>
-            )
-            })
-            
-        }
-        <div className="card cursor-pointer" style={{width: "18rem"}} onClick={() => setShowAddRecipeModal(true)}>
-            <div className="card-body">
-                <i className="add-button fa fa-plus-circle fa-2x" style={{margin: "auto", opacity: "25%"}} aria-hidden="true" ></i>
             </div>
-        </div>
+            <div className="row">
+            {
+                recipes.map((recipe) => {
+                return(
+                    <div key={recipe.id} className="card" style={{width: "18rem"}}>
+                        {/* <img src="..." className="card-img-top" alt="..."></img> */}
+                        <div className="card-body">
+                            <h5 className="card-title">{recipe.recipeName}</h5>
+                            <div>
+                            <p className="card-text"><b>Cuisine Type: </b><span>{recipe.cuisineType}</span></p>
+                            <p className="card-text"><b>Ingredients: </b><span className='cursor-pointer align-self-end' onClick={() => navigate(`/recipe/${recipe.id}`)}>See Recipe <i className="fa fa-chevron-right" aria-hidden="true"></i></span></p>
+                            <p className="card-text"><b>Instructions: </b><span className='cursor-pointer align-self-end'>See Recipe <i className="fa fa-chevron-right" aria-hidden="true"></i></span></p>
+                            </div>
+                        </div>
+                    </div>
+                )
+                })
+                
+            }
+            <div className="card cursor-pointer" style={{width: "18rem"}} onClick={() => setShowAddRecipeModal(true)}>
+                <div className="card-body">
+                    <i className="add-button fa fa-plus-circle fa-2x" style={{margin: "auto", opacity: "25%"}} aria-hidden="true" ></i>
+                </div>
+            </div>
+            </div>
+            
 
-        <Modal show={showAddRecipeModal} onHide={() => setShowAddRecipeModal(false)}  size="lg">
-                <Modal.Header>
-                    <Modal.Title>
-                        Add Receipe
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <div>
-                    <div className='row col-md-12'>
-                        <div className='col-md-6'>
-                        <input type='text' className='form-control' id="name" name="name" placeholder='Receipe Name'  onChange={(event) => dispatch({type: "name", value: event.target.value})}/>
+            <Modal show={showAddRecipeModal} onHide={() => setShowAddRecipeModal(false)}  size="lg">
+                    <Modal.Header>
+                        <Modal.Title>
+                            Add Receipe
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div>
+                        <div className='row col-md-12'>
+                            <div className='col-md-6'>
+                            <input type='text' className='form-control' id="name" name="name" placeholder='Receipe Name'  onChange={(event) => dispatch({type: "name", value: event.target.value})}/>
+                            </div>
+                            <div className='col-md-6'>
+                            <select className='form-control' onClick={(event) => dispatch({type: "type", value: event.target.value})}>
+                                <option value="">Select Cuisine...</option>
+                                <option value="Italian">Italian</option>
+                                <option value="Chinese">Chinese</option>
+                                <option value="American">American</option>
+                            </select>
+                            </div>
                         </div>
-                        <div className='col-md-6'>
-                        <select className='form-control' onClick={(event) => dispatch({type: "type", value: event.target.value})}>
-                            <option value="">Select Cuisine...</option>
-                            <option value="Italian">Italian</option>
-                            <option value="Chinese">Chinese</option>
-                            <option value="American">American</option>
-                        </select>
+                        <div className='row col-md-12'>
+                            <div className='col-md-12'>
+                                <textarea  className='form-control' rows={2} id="ingredients" name="ingredients" placeholder='Ingredients' onChange={(event) => dispatch({type: "ingredients", value: event.target.value})}/>
+                            </div>
                         </div>
-                    </div>
-                    <div className='row col-md-12'>
-                        <div className='col-md-12'>
-                            <textarea  className='form-control' rows={2} id="ingredients" name="ingredients" placeholder='Ingredients' onChange={(event) => dispatch({type: "ingredients", value: event.target.value})}/>
+                        <div className='row col-md-12'>
+                            <div className='col-md-12'>
+                                <textarea  className='form-control' rows={10} id="instructions" name="instructions" placeholder='Cooking Instructions' onChange={(event) => dispatch({type: "instructions", value: event.target.value})}/>
+                            </div>
                         </div>
-                    </div>
-                    <div className='row col-md-12'>
-                        <div className='col-md-12'>
-                            <textarea  className='form-control' rows={10} id="instructions" name="instructions" placeholder='Cooking Instructions' onChange={(event) => dispatch({type: "instructions", value: event.target.value})}/>
                         </div>
-                    </div>
-                    </div>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button type="button" className="btn btn-secondary" onClick={() => setShowAddRecipeModal(false)}>Close</button>
-                    <button type="button" className="btn btn-primary" disabled={handleDisabled()} onClick={() => [dispatch({type: "add"}), setShowAddRecipeModal(false), dispatch({type:"clear"})]}>Add</button>
-                </Modal.Footer>
-            </Modal>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button type="button" className="btn btn-secondary" onClick={() => setShowAddRecipeModal(false)}>Close</button>
+                        <button type="button" className="btn btn-primary" disabled={handleDisabled()} onClick={() => [dispatch({type: "add"}), setShowAddRecipeModal(false), dispatch({type:"clear"})]}>Add</button>
+                    </Modal.Footer>
+                </Modal>
         </div>
     );
     }
--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -1,11 +1,37 @@
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
-
+import { RecipeContext } from "../../context/RecipeContext";
 
 export function Recipe(){
 
     const recipeId = useParams();
+    const {recipes} = useContext(RecipeContext);
+
+    const [recipe, setRecipe] = useState({});
+
+    const getRecipe = () => {
+        console.log(recipes);
+        console.log(recipeId);
+        setRecipe(recipes.find((x) => x.id === recipeId.recipeId));
+    }
+
+
+    useEffect(() => {
+        getRecipe();
+    }, [])
+
 
     return(
-        <div>This is recipe {recipeId}</div>
+        <div className="row d-flex justify-content-center">
+            <div className="d-flex justify-content-center"><h4>{recipe.recipeName}</h4></div>
+
+            <div className="card box-shadow" style={{width: "18rem"}}>
+            <div className="card-body">
+                <p><b>Cuisine: </b>{recipe.cuisineType}</p>
+                <p><b>Ingredients: </b>{recipe.ingredients}</p>
+                <p><b>Instructions: </b>{recipe.ingredients}</p>
+            </div>
+        </div>
+        </div>
     )
 }
